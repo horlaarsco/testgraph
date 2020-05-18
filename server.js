15d1a3a2bddb542f5df27f2535d50dd8e8f09462
Darey <horla@Dareys-MacBook-Pro.local>
2020-05-18 18:08:59 +0100

    dd

--- server.js
+++ server.js
@@ -1,87 +1,78 @@
-var express = require("express");
-var express_graphql = require("express-graphql");
-var { buildSchema } = require("graphql");
+import express from "express";
+import bodyParser from "body-parser";
+import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
+import { makeExecutableSchema } from "graphql-tools";
 
-// GraphQL Schema
-var schema = buildSchema(`
+import { connectToDB } from "./database";
+
+// Start the http server
+const startServer = async () => {
+  const { User } = require("./database/models");
+
+  // GraphQL Types
+  const typeDefs = `
+    type User {
+      _id: ID!
+      name: String
+      password: String
+    }
     type Query {
-        course(id: Int!): Course
-        courses(topic: String): [Course]
+      users: [User]
     }
-   
-    type Course {
-        id: Int
-        title: String
-        author: String
-        description: String
-        topic: String
-        url: String
+    type Mutation {
+      addUser(input: UserInput): User
     }
-`);
+    input UserInput {
+      name: String!
+      password: String!
+    }
+  `;
 
-var coursesData = [
-  {
-    id: 1,
-    title: "The Complete Node.js Developer Course",
-    author: "Andrew Mead, Rob Percival",
-    description:
-      "Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!",
-    topic: "Node.js",
-    url: "https://codingthesmartway.com/courses/nodejs/",
-  },
-  {
-    id: 2,
-    title: "Node.js, Express & MongoDB Dev to Deployment",
-    author: "Brad Traversy",
-    description:
-      "Learn by example building & deploying real-world Node.js applications from absolute scratch",
-    topic: "Node.js",
-    url: "https://codingthesmartway.com/courses/nodejs-express-mongodb/",
-  },
-  {
-    id: 3,
-    title: "JavaScript: Understanding The Weird Parts",
-    author: "Anthony Alicea",
-    description:
-      "An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.",
-    topic: "JavaScript",
-    url: "https://codingthesmartway.com/courses/understand-javascript/",
-  },
-];
+  // GraphQL resolvers
+  const resolvers = {
+    Query: {
+      users: async () => {
+        const res = await User.find();
+        return res;
+      },
+    },
 
-var getCourse = function (args) {
-  var id = args.id;
-  return coursesData.filter((course) => {
-    return course.id == id;
-  })[0];
+    Mutation: {
+      addUser: async (root, args) => {
+        const res = await User.create(args.input);
+        return res;
+      },
+    },
+  };
+
+  // Define a schema
+  const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  });
+
+  // Initiate express and define routes
+  const app = express();
+  app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
+  app.use("/", graphiqlExpress({ endpointURL: "/graphql" }));
+
+  // Initiate the server
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server started on port: ${process.env.PORT || 3000}`);
+  });
 };
 
-var getCourses = function (args) {
-  if (args.topic) {
-    var topic = args.topic;
-    return coursesData.filter((course) => course.topic === topic);
-  } else {
-    return coursesData;
+// Connecting to DB and then start the server
+const dbConnectAndStartServer = async () => {
+  try {
+    await connectToDB();
+    console.log("Connected to Mongo successfully");
+    startServer();
+  } catch (err) {
+    console.error(`Error connecting to mongo - ${err.message}`);
+    process.exit(1);
   }
 };
 
-// Root resolver
-var root = {
-  course: getCourse,
-  courses: getCourses,
-};
-
-// Create an expres server and a GraphQL endpoint
-var app = express();
-app.use(
-  "/",
-  express_graphql({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-  })
-);
-
-app.listen(4000, () =>
-  console.log("Express GraphQL Server Now Running On localhost:4000/graphql")
-);
+// Entry point
+dbConnectAndStartServer();
