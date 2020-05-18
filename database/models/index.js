234c9c1c3685ecd77daae7338b57a10d7ff30983
Darey <horla@Dareys-MacBook-Pro.local>
2020-05-18 18:24:12 +0100

    sddss

---
+++ database/models/index.js
@@ -0,0 +1,18 @@
+const { getDB } = require("../index");
+
+const DB = getDB();
+
+//#region schemas
+const USER_SCHEMA = DB.Schema({
+  name: String,
+  password: String,
+});
+//#endregion schemas
+
+//#region models
+const User = DB.model("User", USER_SCHEMA);
+//#endregion models
+
+module.exports = {
+  User,
+};
